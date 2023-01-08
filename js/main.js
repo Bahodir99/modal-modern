a3245ceca44430e387e116364be97622f82a8e29
Bahodir_99 <bahodirataboyev7@gmail.com>
2023-01-08 10:09:09 +0500

    Evil-EYE

--- js/main.js
+++ js/main.js
@@ -30,18 +30,19 @@ elShadow.addEventListener('click', ()=> {
 var  elInp1 = document.querySelector('.text');
 var  elInp2 = document.querySelector('.pass');
 var  elTitle = document.querySelector('.title')
-
+var elEye = document.querySelector('img')
 
 
 elOpen.addEventListener('click', ()=>{
     var a = elInp1.value;
     var b = elInp2.value;
-    if (a == 'bahodir' && b == 'bahodir'){
+    if (a == 'bahodir' && b == 'bahodir' ){
         elTitle.textContent = 'siz togri kiritdingiz'
         elInp1.classList.add('yes');
         elInp1.classList.remove('no')
         elInp2.classList.add('yes')
         elInp2.classList.remove('no')
+        // elInp2.setAttribute('type', 'text')
     }
     else{
         elTitle.textContent = 'siz xato kiritdingiz'
@@ -49,6 +50,17 @@ elOpen.addEventListener('click', ()=>{
         elInp1.classList.remove('yes')
         elInp2.classList.add('no')
         elInp2.classList.remove('yes')
+        // elInp2.setAttribute('type', 'password')
     }
 })
 
+elEye.addEventListener('click', ()=>{
+    if (elInp2.getAttribute('type') === 'password'){
+        elInp2.setAttribute('type', 'text')
+        elEye.style.opacity = '1'
+    }
+    else{
+        elInp2.setAttribute('type', 'password')
+        elEye.style.opacity = '0.6'
+    }
+})
